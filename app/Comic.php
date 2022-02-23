1514bb004676f101829f11752bab51270bcac98b
ElperdonXV <davidemartinografico@gmail.com>
2022-02-23 15:48:53 +0100

    work on comic

--- app/Comic.php
+++ app/Comic.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comic extends Model
 {
-    //
+    protected $fillable = [
+        
+            'title',
+            'description',
+            'author',
+            'publishing_house',
+            'number_of_pages',
+            'thumb',
+            'edition',
+            'price',
+            'created_at',
+            'updated_at',
+    ];
 }
