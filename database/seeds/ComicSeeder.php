cb158a3b4d7b065cc74e2913533abea3971c15dd
ElperdonXV <davidemartinografico@gmail.com>
2022-02-23 16:14:21 +0100

    seeder done

--- database/seeds/ComicSeeder.php
+++ database/seeds/ComicSeeder.php
@@ -13,6 +13,17 @@ class ComicSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i<20; $i++){
+            $comic = new Comic();
+            $comic->title = $faker->word();
+            $comic->description = $faker->words(3, true);
+            $comic->author = $faker->word();
+            $comic->publishing_house = $faker->word();
+            $comic->number_of_pages = $faker->randomNumber(3, false);
+            $comic->thumb = $faker->imageUrl(640, 480, 'comics', true);
+            $comic->edition = $faker->randomNumber(3, false);
+            $comic->price = $faker->randomFloat(1,29,49);
+            $comic->save();
+        }
     }
 }
