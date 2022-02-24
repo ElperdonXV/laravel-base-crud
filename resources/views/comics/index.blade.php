ae9d4d07da897cf9179a68a1f7bbff33c3b4c852
ElperdonXV <davidemartinografico@gmail.com>
2022-02-24 16:29:56 +0100

    work on design

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,6 +5,20 @@
 @endsection
 
 @section('content')
+    <div class="container-top">
+        <div class="row">
+            <div class="col bg-left">
+              <h1>Welcome to comics DB Shop</h1>
+              <p>
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                  Maiores neque nobis qui possimus aspernatur consequuntur, veniam expedita dignissimos. 
+                  Omnis et earum iusto dolor voluptatibus. Iure fuga quasi adipisci blanditiis aut!
+                </p>
+            </div>
+            <div class="col bg-head">
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="row">
             <h1 class="h1">Admin - All Comics</h1>
@@ -16,7 +30,7 @@
         </div>
         <div class="row">
             <div class="col">
-                 <table class="table table-primary">
+                 <table class="table table-dark">
                     <thead>
                         <tr class="table-primary">
                             <th>Author</th>
@@ -38,8 +52,8 @@
                             <td>{{ $comic->number_of_pages}} </td>
                             <td>{{$comic->price}}€</td>
                             <td>
-                                <a class="btn btn-primary" href="{{ route('comics.show', $comic) }}">View</a>
-                                <a class="btn btn-primary" href="{{ route('comics.edit', $comic) }}">Edit</a>
+                                <a class="btn btn-outline-light" href="{{ route('comics.show', $comic) }}">View</a>
+                                <a class="btn btn-outline-light" href="{{ route('comics.edit', $comic) }}">Edit</a>
                             </td>
                         </tr>
                     @endforeach
